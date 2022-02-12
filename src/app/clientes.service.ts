ee0479f33d5998ef1f43594f1bd62cea6ccbf86b
jose_ademar <joseademarti@gmail.com>
2022-02-12 12:40:24 -0300

    Criado o service e o metodo post para salvar dados do cliente

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './clientes/cliente';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,11 @@ export class ClientesService {
 
   constructor( private http: HttpClient) { 
 
-    
+ }
 
-  }
+    salvarCliente(cliente: Cliente) : Observable<Cliente>{
+      return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+    }
 
     getCliente() : Cliente {
 
