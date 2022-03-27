f5d110fb0f3cd98af213fc568f357d9d6e738b5f
jose_ademar <joseademarti@gmail.com>
2022-03-26 21:09:23 -0300

    Realizada a listagem de clientes na tabela do front-end

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Cliente } from './clientes/cliente';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { Cliente } from './clientes/cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor( private http: HttpClient) { 
+  constructor(private http: HttpClient) { }
 
- }
+  // Metodo para persistir um cliente no banco
+  salvarCliente(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
 
-    salvarCliente(cliente: Cliente) : Observable<Cliente>{
-      return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
-    }
 
-   // getCliente() : Cliente {
-
-      // let cliente : Cliente  = new Cliente();
-      // cliente.nome = 'Fulano de Tal';
-      // cliente.cpf = '888888888';
-      // return cliente;
-   // }
+  listaClientes(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>('http://localhost:8080//api/clientes');
+  }
 }
